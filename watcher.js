55f48559aea0cb76e9177f70ac0ec712441d6c0e
caiyifan5 <caiyifan5@jd.com>
2021-11-29 21:37:22 +0800

    feat: 支持多层级apis生成

--- watcher.js
+++ watcher.js
@@ -19,27 +19,42 @@ const watcher = chokidar.watch(path.resolve(__dirname, config.mockPath), {
 
 watcher.on('change', (key) => {
   let filePath = getFilename(key);
+  console.log(filePath)
   const file = filePath.split('/');
   const fileName = processKeyWord(file[file.length - 1]);
   file[file.length-1] = fileName
   filePath = file.join('/')
   const basePath = path.resolve(__dirname, config.apisPath + filePath + '.ts');
-  const dirPath = path.resolve(__dirname, config.apisPath + '/' + file[1]);
-  const data = require(key);
-  if (fs.existsSync(dirPath)) {
-    !fs.existsSync(basePath) && fs.writeFileSync(basePath, template.getTsTemplate({ fileName, data, config }));
-    const fileGroup = fs
-    .readdirSync(dirPath)
-    .map((item) => item.split('.')[0])
-    .filter((item) => item !== 'index');
-    fs.writeFileSync(dirPath + '/index.ts', template.getIndexTemplate(fileGroup));
-  } else {
-    fs.mkdirSync(dirPath);
-    fs.writeFileSync(basePath, template.getTsTemplate({ fileName, data, config }));
-    const fileGroup = fs
-      .readdirSync(dirPath)
-      .map((item) => item.split('.')[0])
-      .filter((item) => item !== 'index');
-    fs.writeFileSync(dirPath + '/index.ts', template.getIndexTemplate(fileGroup));
+  let initPath = path.resolve(__dirname, config.apisPath + '/');
+  for(let i = 1; i < file.length - 1; i++) {
+    initPath = initPath + '/' + file[i]
+    if(!fs.existsSync(initPath)) {
+      fs.mkdirSync(initPath);
+      // fs.writeFileSync(dirPath + '/index.ts', template.getIndexTemplate(fileGroup));
+    }
   }
+  const data = require(key);
+  // if (fs.existsSync(dirPath)) {
+    !fs.existsSync(basePath) && fs.writeFileSync(basePath, template.getTsTemplate({ fileName, data, config }));
+    let indexPath = basePath.split('/')
+    indexPath.pop();
+    indexPath = indexPath.join('/')
+    const fileGroup = fs
+    .readdirSync(indexPath)
+    .map((item) => {
+      if (item.includes('.ts')) {
+       return item.split('.')[0]
+      }
+    })
+    .filter((item) => item !== 'index');
+    fs.writeFileSync(indexPath + '/index.ts', template.getIndexTemplate(fileGroup));
+  // } else {
+    // fs.mkdirSync(dirPath);
+    // fs.writeFileSync(basePath, template.getTsTemplate({ fileName, data, config }));
+    // const fileGroup = fs
+    //   .readdirSync(dirPath)
+    //   .map((item) => item.split('.')[0])
+    //   .filter((item) => item !== 'index');
+    // fs.writeFileSync(dirPath + '/index.ts', template.getIndexTemplate(fileGroup));
+  // }
 });
