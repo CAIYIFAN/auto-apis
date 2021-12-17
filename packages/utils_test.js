b2c37cd947f5493bc26aa8f0a56b082a2e4e71c3
caiyifan5 <caiyifan5@jd.com>
2021-12-17 19:05:58 +0800

    feat: 修改

--- packages/utils_test.js
+++ packages/utils_test.js
@@ -5,8 +5,24 @@ const fs = require('fs');
 const path = require('path');
 // console.log(fs.readFile(path.resolve('')))
 
-console.log(path.resolve() + '/README.md');
+// console.log(path.resolve() + '/README.md');
 
-console.log(fs.readFile(path.resolve() + '/README.md', 'utf8', (err, data) => {
-  console.log(data.split('##'))
-}))
+// console.log(fs.readFile(path.resolve() + '/README.md', 'utf8', (err, data) => {
+//   console.log(data.split('##'))
+// }))
+
+const { unified } = require('')
+
+
+main()
+
+async function main() {
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeSanitize)
+    .use(rehypeStringify)
+    .process('# Hello, Neptune!')
+
+  console.log(String(file))
+}
